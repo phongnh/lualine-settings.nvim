2ac15a63621e898eff79ccd8e20b068c58725cb0
Phong Nguyen <nhphong1406@gmail.com>
2024-09-24 13:30:00 +0700

    Add CtrlP extension

---
+++ autoload/lualine_settings/ctrlp.vim
@@ -0,0 +1,26 @@
+let g:lualine_ctrlp = { 'name': 'CtrlP' }
+
+function! s:GetCurrentDir() abort
+    let dir = fnamemodify(getcwd(), ':~:.')
+    return empty(dir) ? getcwd() : dir
+endfunction
+
+function! lualine_settings#ctrlp#MainStatus(focus, byfname, regex, prev, item, next, marked) abort
+    let g:lualine_ctrlp.main    = v:true
+    let g:lualine_ctrlp.focus   = a:focus
+    let g:lualine_ctrlp.byfname = a:byfname
+    let g:lualine_ctrlp.regex   = a:regex
+    let g:lualine_ctrlp.prev    = a:prev
+    let g:lualine_ctrlp.item    = a:item
+    let g:lualine_ctrlp.next    = a:next
+    let g:lualine_ctrlp.marked  = a:marked
+    let g:lualine_ctrlp.dir     = s:GetCurrentDir()
+    return v:lua.require("lualine").statusline()
+endfunction
+
+function! lualine_settings#ctrlp#ProgressStatus(len) abort
+    let g:lualine_ctrlp.main = v:false
+    let g:lualine_ctrlp.len  = a:len
+    let g:lualine_ctrlp.dir  = s:GetCurrentDir()
+    return v:lua.require("lualine").statusline()
+endfunction
---
+++ lua/lualine/extensions/ctrlp.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+M.sections = {
+  lualine_a = { "g:lualine_ctrlp.name" },
+  lualine_b = {
+    {
+      "g:lualine_ctrlp.prev",
+      cond = function()
+        return vim.g.lualine_ctrlp.main
+      end,
+    },
+    {
+      "g:lualine_ctrlp.item",
+      cond = function()
+        return vim.g.lualine_ctrlp.main
+      end,
+      fmt = function(name, _context)
+        return string.format("« %s »", name)
+      end,
+    },
+    {
+      "g:lualine_ctrlp.next",
+      cond = function()
+        return vim.g.lualine_ctrlp.main
+      end,
+    },
+  },
+  lualine_y = {
+    {
+      "g:lualine_ctrlp.focus",
+      cond = function()
+        return vim.g.lualine_ctrlp.main
+      end,
+    },
+    {
+      "g:lualine_ctrlp.byfname",
+      cond = function()
+        return vim.g.lualine_ctrlp.main
+      end,
+    },
+    {
+      "g:lualine_ctrlp.len",
+      cond = function()
+        return not vim.g.lualine_ctrlp.main
+      end,
+    },
+  },
+  lualine_z = { "g:lualine_ctrlp.dir" },
+}
+
+M.filetypes = { "ctrlp" }
+
+return M
