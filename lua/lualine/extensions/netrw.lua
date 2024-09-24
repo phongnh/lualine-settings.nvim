425eeffddae8bbccc1d4608c65c4068510646825
Phong Nguyen <nhphong1406@gmail.com>
2024-09-24 15:32:17 +0700

    Add netrw extension

---
+++ lua/lualine/extensions/netrw.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.sections = {
+  lualine_a = {
+    function()
+      return "Netrw"
+    end,
+  },
+  lualine_b = {
+    {
+      "b:netrw_curdir",
+      fmt = function(name, _context)
+        return vim.fn.fnamemodify(name, ":p:~:h")
+      end,
+    },
+  },
+  lualine_y = {
+    {
+      function()
+        return vim.g.netrw_sort_direction or "n"
+      end,
+      fmt = function(name, _context)
+        return (name == "normal" or name == "n") and "[+]" or "[-]"
+      end,
+    },
+  },
+  lualine_z = {
+    "g:netrw_sort_by",
+  },
+}
+
+M.filetypes = { "netrw" }
+
+return M
