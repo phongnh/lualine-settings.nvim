f969d45fd9b93ea68ec09af42d06719e874f9d53
Phong Nguyen <nhphong1406@gmail.com>
2025-02-23 19:34:31 +0700

    Integrate with telescope.nvim plugin

---
+++ lua/lualine/extensions/telescope.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.sections = {
+  lualine_a = {
+    function()
+      return "Telescope"
+    end,
+  },
+  lualine_y = {
+    function()
+      local entry = require("telescope.actions.state").get_selected_entry()
+      return entry and entry[1] or ""
+    end,
+  },
+  lualine_z = {
+    function()
+      local picker = require("telescope.actions.state").get_current_picker(vim.api.nvim_get_current_buf())
+      return picker and picker.prompt_title or ""
+    end,
+  },
+}
+
+M.filetypes = { "TelescopePrompt" }
+
+return M
