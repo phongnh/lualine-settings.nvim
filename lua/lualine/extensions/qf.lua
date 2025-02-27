783f464f90056d21efcdef07a38aec6ee1964782
Phong Nguyen <nhphong1406@gmail.com>
2025-02-27 10:17:15 +0700

    Add custom qf integration

---
+++ lua/lualine/extensions/qf.lua
@@ -0,0 +1,47 @@
+local function is_loclist()
+  return vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0
+end
+
+local function label()
+  return is_loclist() and "Location" or "Quickfix"
+end
+
+local function qf_title()
+  if is_loclist() then
+    return vim.fn.getloclist(0, { title = 0 }).title
+  end
+  return vim.fn.getqflist({ title = 0 }).title
+end
+
+local function title()
+  local text = qf_title()
+  local maxlen = vim.o.columns - string.len(label()) - 2
+  if #text > maxlen then
+    local cmd = ""
+    for _, part in ipairs(vim.fn.split(text, "")) do
+      if string.len(cmd .. " " .. part) > (maxlen - 3) then
+        break
+      end
+      cmd = cmd .. " " .. part
+    end
+    text = cmd .. " " .. LualineSettings.symbols.ellipsis
+  end
+  return text
+end
+
+local M = {}
+
+function M.init()
+  -- Make sure ft wf doesn't create a custom statusline
+  vim.g.qf_disable_statusline = true
+end
+
+M.sections = {
+  lualine_a = { label },
+  lualine_b = { title },
+  lualine_z = { "location" },
+}
+
+M.filetypes = { "qf" }
+
+return M
