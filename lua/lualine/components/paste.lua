ed168fddf4d7d7dfd183a8514106b6d37e57fd35
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 23:09:12 +0700

    Simplify paste component

--- lua/lualine/components/paste.lua
+++ lua/lualine/components/paste.lua
@@ -1,15 +1,8 @@
-local lualine_require = require("lualine_require")
-local M = lualine_require.require("lualine.component"):extend()
-
-function M:init(options)
-  M.super.init(self, options)
-  self.options.cond = function()
-    return vim.o.paste
+local function paste()
+  if vim.o.paste then
+    return LualineSettings.symbols.paste or "[P]"
   end
+  return ""
 end
 
-function M:update_status()
-  return LualineSettings.symbols.paste
-end
-
-return M
+return paste
