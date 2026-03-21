2ce063b345aa3f49adbd0a798a8490b190f02602
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 23:09:39 +0700

    Simplify spell component

--- lua/lualine/components/spell.lua
+++ lua/lualine/components/spell.lua
@@ -1,15 +1,8 @@
-local lualine_require = require("lualine_require")
-local M = lualine_require.require("lualine.component"):extend()
-
-function M:init(options)
-  M.super.init(self, options)
-  self.options.cond = function()
-    return vim.o.spell
+local function spell()
+  if vim.bo.spell then
+    return string.upper((vim.o.spelllang or ""):gsub(",", "/"))
   end
+  return ""
 end
 
-function M:update_status()
-  return string.upper((vim.o.spelllang or ""):gsub(",", "/"))
-end
-
-return M
+return spell
