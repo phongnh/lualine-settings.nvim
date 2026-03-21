83c8e1cfed4508c2474e7a938fd9719fb889cf82
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 23:08:52 +0700

    Simplify clipboard component

--- lua/lualine/components/clipboard.lua
+++ lua/lualine/components/clipboard.lua
@@ -1,15 +1,8 @@
-local lualine_require = require("lualine_require")
-local M = lualine_require.require("lualine.component"):extend()
-
-function M:init(options)
-  M.super.init(self, options)
-  self.options.cond = function()
-    return string.find(vim.o.clipboard or "", "unnamed") ~= nil
+local function clipboard()
+  if string.find(vim.o.clipboard, "unnamed") ~= nil then
+    return LualineSettings.symbols.clipboard or "[C]"
   end
+  return ""
 end
 
-function M:update_status()
-  return LualineSettings.symbols.clipboard
-end
-
-return M
+return clipboard
