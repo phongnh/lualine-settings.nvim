3bac526894bcc348d1793f0dc8ef3a2b50926d39
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 23:15:47 +0700

    Fix indent component

--- lua/lualine/components/indent.lua
+++ lua/lualine/components/indent.lua
@@ -1,4 +1,4 @@
-local function indent() abort
+local function indent()
   local is_compact = vim.api.nvim_win_get_width(0) < 100
   if vim.bo.expandtab then
     local shiftwidth = vim.fn.exists("*shiftwidth") == 1 and vim.fn.shiftwidth() or vim.bo.shiftwidth
