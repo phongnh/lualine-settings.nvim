fe45d376ed984792aabd69086ecfb39678c818f9
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 23:10:51 +0700

    Simplify indent component

--- lua/lualine/components/indent.lua
+++ lua/lualine/components/indent.lua
@@ -1,15 +1,5 @@
-local lualine_require = require("lualine_require")
-local M = lualine_require.require("lualine.component"):extend()
-
-function M:init(options)
-  M.super.init(self, options)
-  self.options.is_compact = function()
-    return vim.api.nvim_win_get_width(0) < 100
-  end
-end
-
-function M:update_status()
-  local is_compact = self.options.is_compact()
+local function indent() abort
+  local is_compact = vim.api.nvim_win_get_width(0) < 100
   if vim.bo.expandtab then
     local shiftwidth = vim.fn.exists("*shiftwidth") == 1 and vim.fn.shiftwidth() or vim.bo.shiftwidth
     return string.format(is_compact and "SPC: %d" or "Spaces: %d", shiftwidth)
@@ -18,4 +8,4 @@ function M:update_status()
   end
 end
 
-return M
+return indent
