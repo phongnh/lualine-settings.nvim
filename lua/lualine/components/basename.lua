55a83002fb33d7a8b379b88c94ffe0e638b44eb6
Phong Nguyen <nhphong1406@gmail.com>
2026-03-21 21:33:35 +0700

    Simplify basename component

--- lua/lualine/components/basename.lua
+++ lua/lualine/components/basename.lua
@@ -1,12 +1,5 @@
-local lualine_require = require("lualine_require")
-local M = lualine_require.require("lualine.component"):extend()
-
-function M:init(options)
-  M.super.init(self, options)
+local function basename()
+  vim.fs.basename(vim.api.nvim_buf_get_name(0))
 end
 
-function M:update_status()
-  return vim.fs.basename(vim.api.nvim_buf_get_name(0))
-end
-
-return M
+return basename
