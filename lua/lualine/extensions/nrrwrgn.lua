02f5f8748d71b18d98dfbca33752c012a969938f
Phong Nguyen <nhphong1406@gmail.com>
2026-03-22 13:46:42 +0700

    Refactor: NrrwRgn extension

--- lua/lualine/extensions/nrrwrgn.lua
+++ lua/lualine/extensions/nrrwrgn.lua
@@ -1,49 +1,49 @@
 -- https://github.com/chrisbra/NrrwRgn
 local M = {}
 
-local get_nrrw_rgn_status = function()
-  return vim.fn.exists("*nrrwrgn#NrrwRgnStatus") == 1 and vim.fn["nrrwrgn#NrrwRgnStatus"]() or vim.empty_dict()
+local visual_modes = {
+  [""] = "",
+  v = " [C]",
+  V = "",
+  [""] = " [B]",
+}
+
+local function mode()
+  local name = vim.b.nrrw_instn ~= nil and string.format("NrrwRgn#%d", vim.b.nrrw_instn) or "NrrwRgn"
+  local prefix = vim.startswith(vim.fn.bufname("%"), "NrrwRgn_multi") and "Multi" or ""
+
+  local status = vim.fn["nrrwrgn#NrrwRgnStatus"]()
+  if vim.tbl_isempty(status) then
+    return prefix .. name
+  end
+  prefix = status.multi == 1 and "Multi" or ""
+  local visual = visual_modes[status.visual or "V"]
+  return prefix .. name .. visual
+end
+
+local function line_range()
+  local status = vim.fn["nrrwrgn#NrrwRgnStatus"]()
+  if status.multi ~= 1 then
+    return string.format("[%d-%d]", status.start[2], status["end"][2])
+  end
+  return ""
+end
+
+local function bufname()
+  local status = vim.fn["nrrwrgn#NrrwRgnStatus"]()
+  local bufname = status.fullname ~= nil and status.fullname or vim.api.nvim_buf_get_name(vim.b.orig_buf or 0)
+  return vim.fn.fnamemodify(bufname, ":~:.")
 end
 
 M.sections = {
   lualine_a = {
-    function()
-      local name = "NrrwRgn"
-
-      if vim.fn.exists(":WR") == 2 or vim.fn.exists(":WidenRegion") == 2 then
-        if vim.b.nrrw_instn ~= nil then
-          name = string.format("%s#%d", "NrrwRgn", vim.b.nrrw_instn)
-        else
-          name = vim.fn.substitute(vim.api.nvim_buf_get_name(0), "^NrrwRgn_\\zs.*\\ze_d+$", submatch(0), "")
-          name = vim.fn.substitute(name, "__", "#", "")
-        end
-
-        local dict = get_nrrw_rgn_status()
-        if not vim.tbl_isempty(dict) then
-          local vmode = { [""] = "", v = " [C]", V = "", [""] = " [B]" }
-          name = (dict.multi == 1 and "Multi" or "") .. name .. vmode[dict.visual or "V"]
-        end
-      end
-
-      return name
-    end,
+    mode,
   },
   lualine_b = {
-    {
-      function()
-        local dict = get_nrrw_rgn_status()
-        if not vim.tbl_isempty(dict) then
-          return vim.fn.fnamemodify(dict.fullname, ":~:.")
-            .. (dict.multi == 1 and "" or string.format(" [%d-%d]", dict.start[2], dict["end"][2]))
-        elseif vim.b.orig_buf ~= nil then
-          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.b.orig_buf), ":~:.")
-        end
-        return ""
-      end,
-      cond = function()
-        return vim.fn.exists(":WR") == 2 or vim.fn.exists(":WidenRegion") == 2
-      end,
-    },
+    line_range,
+  },
+  lualine_c = {
+    bufname,
   },
   lualine_z = { "location" },
 }
