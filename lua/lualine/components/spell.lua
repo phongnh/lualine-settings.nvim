ef9a9ef651b158b0026ecb6b75cb9815af1add4d
Phong Nguyen <nhphong1406@gmail.com>
2026-04-11 14:32:06 +0700

    Handle multiple spelllang

--- lua/lualine/components/spell.lua
+++ lua/lualine/components/spell.lua
@@ -1,6 +1,16 @@
+local spelllang_maps = {
+  ["en_us"] = "US",
+  ["en_gb"] = "GB",
+}
+
 local function spell()
   if vim.o.spell then
-    return string.upper((vim.o.spelllang or ""):gsub(",", "/"))
+    return vim
+      .iter(vim.split(vim.o.spelllang or "", ",", { plain = true, trimempty = true }))
+      :map(function(val)
+        return spelllang_maps[val] or string.upper(val)
+      end)
+      :join("/")
   end
   return ""
 end
