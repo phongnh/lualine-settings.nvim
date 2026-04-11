0650702c56f34cbde0f4df1113ea7aabee855df4
Phong Nguyen <nhphong1406@gmail.com>
2026-04-11 10:48:59 +0700

    Fix vim plugin integrations

--- plugin/lualine_settings.lua
+++ plugin/lualine_settings.lua
@@ -15,32 +15,26 @@ vim.api.nvim_create_autocmd("UIEnter", {
   pattern = "*",
   callback = function()
     -- CtrlP Integration
-    if vim.fn.exists(":CtrlP") == 2 then
-      vim.g.ctrlp_status_func = {
-        main = "lualine_settings#ctrlp#MainStatus",
-        prog = "lualine_settings#ctrlp#ProgressStatus",
-      }
-    end
+    vim.g.ctrlp_status_func = {
+      main = "lualine_settings#ctrlp#MainStatus",
+      prog = "lualine_settings#ctrlp#ProgressStatus",
+    }
 
     -- CtrlSF Integration
-    if vim.fn.exists(":CtrlSF") == 2 then
-      vim.api.nvim_create_autocmd("BufEnter", {
-        group = vim.api.nvim_create_augroup("LualineSettingsCtrlSFPreview", { clear = true }),
-        pattern = "__CtrlSFPreview__",
-        callback = function(evt)
-          if string.find(vim.bo[evt.buf].filetype, "ctrlsf-preview") ~= nil then
-            return
-          end
-          local filetype = vim.fn.fnamemodify(vim.b.ctrlsf_file, ":e")
-          vim.bo[evt.buf].filetype = filetype .. ".ctrlsf-preview"
-        end,
-      })
-    end
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("LualineSettingsCtrlSFPreview", { clear = true }),
+      pattern = "__CtrlSFPreview__",
+      callback = function(evt)
+        if string.find(vim.bo[evt.buf].filetype, "ctrlsf-preview") ~= nil then
+          return
+        end
+        local filetype = vim.fn.fnamemodify(vim.b.ctrlsf_file, ":e")
+        vim.bo[evt.buf].filetype = filetype .. ".ctrlsf-preview"
+      end,
+    })
 
     -- Tagbar Integration
-    if vim.fn.exists(":Tagbar") == 2 then
-      vim.g.tagbar_status_func = "lualine_settings#tagbar#Status"
-    end
+    vim.g.tagbar_status_func = "lualine_settings#tagbar#Status"
   end,
 })
 
