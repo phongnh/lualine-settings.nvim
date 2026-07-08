1637febb52c8dd003a6a6b156192fcf28e6d05e4
Phong Nguyen <nhphong1406@gmail.com>
2026-07-08 23:36:25 +0700

    Improve qf extension to escape %

--- lua/lualine/extensions/qf.lua
+++ lua/lualine/extensions/qf.lua
@@ -24,21 +24,23 @@ end
 M.sections = {
   lualine_a = { label },
   lualine_b = {
-    function()
-      local text = title()
-      local maxlen = vim.o.columns - string.len(label()) - 2
-      if #text > maxlen then
-        local cmd = ""
-        for _, part in ipairs(vim.fn.split(text, "")) do
-          if string.len(cmd .. " " .. part) > (maxlen - 3) then
-            break
+    {
+      title,
+      fmt = function(text)
+        local maxlen = vim.o.columns - string.len(label()) - 2
+        if #text > maxlen then
+          local cmd = ""
+          for _, part in ipairs(vim.fn.split(text, "")) do
+            if string.len(cmd .. " " .. part) > (maxlen - 3) then
+              break
+            end
+            cmd = cmd .. " " .. part
           end
-          cmd = cmd .. " " .. part
+          text = cmd .. " " .. LualineSettings.symbols.ellipsis
         end
-        text = cmd .. " " .. LualineSettings.symbols.ellipsis
-      end
-      return text
-    end,
+        return text:gsub("%%", "%%%%")
+      end,
+    },
   },
   lualine_z = { "location" },
 }
