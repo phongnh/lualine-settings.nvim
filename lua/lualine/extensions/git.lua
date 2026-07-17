bd35da35032d9cbbf60a4f66941b739a1bc0d76a
Phong Nguyen <nhphong1406@gmail.com>
2026-07-17 19:17:08 +0700

    fugitive: auto set b:fugitive_git_command based on FugitiveChanged event

--- lua/lualine/extensions/git.lua
+++ lua/lualine/extensions/git.lua
@@ -8,16 +8,14 @@ M.sections = {
   },
   lualine_c = {
     function()
-      if
-        vim.g._fugitive_last_job ~= nil
-        and (
-          vim.g._fugitive_last_job.file == vim.api.nvim_buf_get_name(0)
-          or vim.g._fugitive_last_job.capture_bufnr == vim.api.nvim_get_current_buf()
-        )
+      if vim.b.fugitive_git_command ~= nil then
+        return vim.b.fugitive_git_command
+      elseif
+        vim.g._fugitive_last_job ~= nil and vim.g._fugitive_last_job.capture_bufnr == vim.api.nvim_get_current_buf()
       then
-        local cmd = vim.list_extend({}, vim.g._fugitive_last_job.git)
-        cmd = vim.list_extend(cmd, vim.g._fugitive_last_job.args)
-        return vim.iter(cmd):join(" ")
+        local cmd = vim.iter({ vim.g._fugitive_last_job.git, vim.g._fugitive_last_job.args }):flatten():join(" ")
+        vim.b.fugitive_git_command = cmd
+        return cmd
       end
       return vim.fs.basename(vim.api.nvim_buf_get_name(0))
     end,
