90fdfa1a251e0ff4b7af46c3cfe4ead4efdaa92d
Phong Nguyen <nhphong1406@gmail.com>
2026-07-27 15:20:45 +0700

    Improve Fugitive integration

--- plugin/lualine_settings.lua
+++ plugin/lualine_settings.lua
@@ -7,19 +7,3 @@ vim.api.nvim_create_autocmd("CmdwinEnter", {
     vim.o.syntax = "vim"
   end,
 })
-
-vim.api.nvim_create_autocmd("User", {
-  group = "LualineSettingsIntegrations",
-  pattern = "FugitiveChanged",
-  callback = function()
-    if not vim.g._fugitive_last_job then
-      return
-    end
-
-    local bufnr = vim.g._fugitive_last_job.capture_bufnr or -1
-    if bufnr > 0 then
-      vim.b[bufnr].fugitive_git_command =
-        vim.iter({ vim.g._fugitive_last_job.git, vim.g._fugitive_last_job.args }):flatten():join(" ")
-    end
-  end,
-})
